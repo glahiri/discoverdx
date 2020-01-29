00b9f5177b6c486bcfae9939b80a5d1bda6b4bf8
Gourab Lahiri <Gourab.Lahiri@gmail.com>
2020-01-30 00:47:18 +0530

    removing cdn dependency on semantic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import PegaApp from './_components/PegaApp';
 import {store} from './store';
-
-const styleLink = document.createElement("link");
-styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
-document.head.appendChild(styleLink);
+import 'semantic-ui-css/semantic.min.css'
 
 
 function App() {
